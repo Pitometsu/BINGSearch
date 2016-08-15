539a97de7009f16e46682a0794486bd1712c2000
Yuriy Pitomets <pitometsu@gmail.com>
2016-08-15 19:11:28 +0300

    Initial Commit

---
+++ BINGSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BINGSearch
+//
+//  Created by Yuriy Pitomets on 8/15/16.
+//  Copyright © 2016 Yuriy Pitomets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BINGSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BINGSearch
+//
+//  Created by Yuriy Pitomets on 8/15/16.
+//  Copyright © 2016 Yuriy Pitomets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
